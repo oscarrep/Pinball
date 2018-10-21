f3a723ba085433cd7b24a6762478e7588d90a234
oscarrep <oscar.bcn.95@gmail.com>
2018-10-21 18:15:53 +0200

    changed screen size

--- Pinball/Globals.h
+++ Pinball/Globals.h
@@ -21,12 +21,12 @@ enum update_status
 };
 
 // Configuration -----------
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 768
+#define SCREEN_WIDTH 609
+#define SCREEN_HEIGHT 907
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN false
-#define WIN_RESIZABLE false
+#define WIN_RESIZABLE true
 #define WIN_BORDERLESS false
 #define WIN_FULLSCREEN_DESKTOP false
 #define VSYNC true
-#define TITLE "2D Physics Playground"
+#define TITLE "RAINBOW PINBALL"
