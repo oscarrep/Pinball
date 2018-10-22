c46cbbaa4e1a707580baaee8f2b40c8f3cc69751
oscarrep <oscar.bcn.95@gmail.com>
2018-10-22 15:50:06 +0200

    fixing gitignore

--- Pinball/ModuleSceneIntro.cpp
+++ Pinball/ModuleSceneIntro.cpp
@@ -56,10 +56,10 @@ update_status ModuleSceneIntro::Update()
 		circles.getLast()->data->listener = this;
 	}
 
-	if(App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN)
+	/*if(App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN)
 	{
 		boxes.add(App->physics->CreateRectangle(App->input->GetMouseX(), App->input->GetMouseY(), 100, 50));
-	}
+	}*/
 
 	if(App->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN)
 	{
